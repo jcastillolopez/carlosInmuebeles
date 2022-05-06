514f111e1f868d9b290f1fe518f0ac84c63ea9c3
Julian <j.castillo@gestionescastillo.com>
2022-05-07 01:34:28 +0200

    inmuebles form y generalizacion de url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Registro_inmuebleComponent } from './components/registros/inmuebles/registro_inmueble/registro_inmueble.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/' },
+
+  { path: 'registro/inmueble', component: Registro_inmuebleComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { Registro_inmuebleComponent } from './components/registros/inmuebles/registro_inmueble/registro_inmueble.component';
 
 
 @NgModule({
   declarations: [	
     AppComponent,
+    Registro_inmuebleComponent
      
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
