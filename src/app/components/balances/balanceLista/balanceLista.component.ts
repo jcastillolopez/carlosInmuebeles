0feee1d22c7fdd3cd65cf466476ffd0c9eeefae9
Julian <j.castillo@gestionescastillo.com>
2022-05-27 19:07:28 +0200

    cambios papa 27/05/2022

--- src/app/components/balances/balanceLista/balanceLista.component.ts
+++ src/app/components/balances/balanceLista/balanceLista.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ingresogastointerface } from 'src/app/interfaces/ingresoGasto';
+import { ingresogastodetalleinterface } from 'src/app/interfaces/ingresoGastoDetalle';
+import { Globales } from 'src/app/services/Globales.service';
+import { tiposService } from 'src/app/services/tipos.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'balanceLista',
@@ -6,10 +12,166 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./balanceLista.component.css']
 })
 export class BalanceListaComponent implements OnInit {
+  arrIngresosGastosMostrar: ingresogastointerface[];
+  arrIngresosGastosFiltrados: ingresogastointerface[];
+  arrIngresosGastosTodos: ingresogastointerface[];
+  arrIngresosGastosInmuebles: ingresogastointerface[];
+  arrIngresosGastosAnios: ingresogastointerface[];
+  arrInGaDetalle: ingresogastodetalleinterface[];
+  arrListaInmuebles: any[];
+  selectInmuebles: any[];
+  selectAnio: any[];
+  selectMes: any[];
+  inmuebleFiltrado: number;
 
-  constructor() { }
+  constructor(
+    private metodosGlobales: Globales,
+    private metodosTipos: tiposService,
+    private router: Router,
+    private activateRouter: ActivatedRoute,
+  ) { 
+    this.arrIngresosGastosMostrar = [];
+    this.arrIngresosGastosFiltrados = [];
+    this.arrIngresosGastosTodos = [];
+    this.arrIngresosGastosInmuebles = [];
+    this.arrIngresosGastosAnios = [];
+    this.arrListaInmuebles = [];
+    this.selectInmuebles = [];
+    this.selectAnio = [];
+    this.selectMes = [];
+    this.inmuebleFiltrado = 0;
+  }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.arrIngresosGastosTodos = await this.metodosGlobales.getAll(environment.APIPATH_INGRESOGASTOGENERAL + parseInt(sessionStorage.getItem('administradorId')!));
+    this.arrIngresosGastosMostrar = this.arrIngresosGastosTodos;
+    this.arrListaInmuebles = await this.metodosGlobales.getAll(environment.APIPATH_INMUEBLE + parseInt(sessionStorage.getItem('administradorId')!));
+    this.selectInmuebles = this.arrListaInmuebles;
+    this.selectAnio = await this.metodosGlobales.getAll(environment.APIPATH_FACTURASANIO + parseInt(sessionStorage.getItem('administradorId')!));
+    this.selectMes = [
+      {
+        "mes": "Enero",
+        "numero": "1"
+      },
+      {
+        "mes": "Febrero",
+        "numero": "2"
+      },
+      {
+        "mes": "Marzo",
+        "numero": "3"
+      },
+      {
+        "mes": "Abril",
+        "numero": "4"
+      },
+      {
+        "mes": "Mayo",
+        "numero": "5"
+      },
+      {
+        "mes": "Junio",
+        "numero": "6"
+      },
+      {
+        "mes": "Julio",
+        "numero": "7"
+      },
+      {
+        "mes": "Agosto",
+        "numero": "8"
+      },
+      {
+        "mes": "Septiembre",
+        "numero": "9"
+      },
+      {
+        "mes": "Octubre",
+        "numero": "10"
+      },
+      {
+        "mes": "Noviembre",
+        "numero": "11"
+      },
+      {
+        "mes": "Dicienmbre",
+        "numero": "12"
+      }
+]
+    for (const ingresoGasto of this.arrIngresosGastosMostrar) {
+      if (ingresoGasto.inmuebleId != null) {
+        for (const inmueble of this.arrListaInmuebles) {
+          if (ingresoGasto.inmuebleId === inmueble.idInmueble) {
+            ingresoGasto.aliasInmueble = inmueble.alias;
+          }
+        }
+      }
+      if(ingresoGasto.totalGasto != 0){
+        ingresoGasto.totalImporte = ingresoGasto.totalGasto;
+      }else{
+        ingresoGasto.totalImporte = ingresoGasto.totalIngreso;
+      }
+    }
+  }
+  async navegar(idIngresoGasto: number) {
+    this.router.navigate(["/inga/detalle/" + idIngresoGasto])
+    this.arrInGaDetalle = await this.metodosGlobales.getById(environment.APIPATH_INGRESOGASTOESPECIFICO, idIngresoGasto);
+  }
+  detalle(inGaId: number, idInGa: number): boolean {
+    if (inGaId === idInGa) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  inmuebles($event) {
+    this.arrIngresosGastosFiltrados = [];
+    if ($event.target.value != 0) {
+      for (const inmuebles of this.arrIngresosGastosTodos) {
+        if (inmuebles.inmuebleId == $event.target.value) {
+          this.arrIngresosGastosFiltrados.push(inmuebles);
+        }
+      }
+      this.arrIngresosGastosMostrar = this.arrIngresosGastosFiltrados;
+    } else {
+      this.arrIngresosGastosMostrar = this.arrIngresosGastosTodos;
+    }
+    this.arrIngresosGastosInmuebles = this.arrIngresosGastosFiltrados;
+  }
+  anios($event) {
+    this.arrIngresosGastosFiltrados = []
+    if (this.arrIngresosGastosInmuebles.length == 0) {
+      this.arrIngresosGastosInmuebles = this.arrIngresosGastosTodos
+    }
+    if ($event.target.value != 0) {
+      for (const fechas of this.arrIngresosGastosInmuebles) {
+        if (fechas.fechaFactura.toString().substring(0, 4) === $event.target.value) {
+          this.arrIngresosGastosFiltrados.push(fechas);
+        }
+      }
+      this.arrIngresosGastosMostrar = this.arrIngresosGastosFiltrados;
+    } else {
+      this.arrIngresosGastosMostrar = this.arrIngresosGastosInmuebles;
+    }
+    this.arrIngresosGastosAnios = this.arrIngresosGastosFiltrados;
+  }
+  mes($event) {
+    this.arrIngresosGastosFiltrados = []
+    if (this.arrIngresosGastosAnios.length == 0) {
+      this.arrIngresosGastosAnios = this.arrIngresosGastosTodos
+    }
+    if ($event.target.value != 0) {
+      for (const fechas of this.arrIngresosGastosAnios) {
+        console.log(fechas.fechaFactura.toString().substring(5, 7))
+        if (fechas.fechaFactura.toString().substring(5, 7) === $event.target.value) {
+          this.arrIngresosGastosFiltrados.push(fechas);
+        }
+      }
+      this.arrIngresosGastosMostrar = this.arrIngresosGastosFiltrados;
+    } else {
+      this.arrIngresosGastosMostrar = this.arrIngresosGastosAnios;
+    }
+    this.arrIngresosGastosAnios = this.arrIngresosGastosFiltrados;
   }
 
 }
