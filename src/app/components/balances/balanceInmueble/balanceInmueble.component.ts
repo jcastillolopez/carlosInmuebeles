0c4020d96d08a3678b626e8025c329911f908b74
EMEAAD\ccastillo <carlos.castillo2@soprasteria.com>
2022-05-29 13:21:45 +0200

    balance terminado a falta de CSS

---
+++ src/app/components/balances/balanceInmueble/balanceInmueble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-balanceInmueble',
+  templateUrl: './balanceInmueble.component.html',
+  styleUrls: ['./balanceInmueble.component.css']
+})
+export class BalanceInmuebleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
