6d3b915745f767521b8cec4021fca98ef196b538
EMEAAD\ccastillo <carlos.castillo2@soprasteria.com>
2022-06-09 09:35:06 +0200

    mods para permitir la subida a heroku

--- src/app/services/Globales.service.ts
+++ src/app/services/Globales.service.ts
@@ -8,8 +8,8 @@ import { environment } from 'src/environments/environment';
 })
 
 export class Globales {
-  // url: string = process.env.APIPATH_GENERAL;
-  url: string = 'https://gestioninmuebles-spring.herokuapp.com/api/v1/'
+  url: string = process.env.APIPATH_GENERAL;
+  // url: string = 'https://gestioninmuebles-spring.herokuapp.com/api/v1/'
   // url: string = 'http://localhost:8081/api/v1/';
   constructor(private httpClient: HttpClient) {
   }
